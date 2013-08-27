ddcd239588ef075ec19b13ccda499a3eac03c34b
JC Brand <jc@opkode.com>
2013-08-27 09:50:30 +0200

    Add jed.js

---
+++ jed-0.5.4.js
@@ -0,0 +1,7 @@
+{
+    "bungle": {
+        "uri": "https://github.com/SlexAxton/Jed/archive/v0.5.4.tar.gz",
+        "sha1": "38d2d7a8a0cc8c17b1b0bb3ecf26231d9f45f3fe",
+        "main": "jed.js"
+    }
+}
